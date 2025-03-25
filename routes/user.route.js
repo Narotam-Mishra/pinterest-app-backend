f81f9f38249c04009488fb206c2dcdb5ebdada79
Narotam-Mishra <narotam21jan@gmail.com>
2025-03-25 06:09:44 +0530

    pinterest-backend_added_get/post/patch/delete_apis_to_test_1.5

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,21 +2,28 @@
 import express from 'express';
 import { test } from '../controllers/user.controller.js';
 import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
 
 const router = express.Router();
 
 router.post("/create", async (req, res) => {
     const userInfo = req.body;
-    console.log("User Information:", userInfo);
-    
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    // console.log("Hashed Password:", hashedPassword);
+
     await User.create({
         displayName: req.body.displayName,
         username: req.body.username,
         email: req.body.email,
-        // hashedPassword: req.body
+        hashedPassword: hashedPassword,
     })
     res.json("user created!!");
-})
+});
+
+router.delete("/delete", async (req, res) => {
+    const delUser = await User.deleteOne({ username: "Mike"})
+    res.json(delUser);
+});
 
 router.get("/test", test)
 

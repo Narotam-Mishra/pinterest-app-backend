cc03cdda9b03d354b895595a646bab6baf977a82
Narotam-Mishra <narotam21jan@gmail.com>
2025-04-03 08:13:26 +0530

    pinterest-backend_added_jwt_for_auth_p1_2.8

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const registerUser = async (req, res) => {
     const { username, displayName, email, password } = req.body;
@@ -43,10 +44,23 @@ export const loginUser = async (req, res) => {
     if(!isPasswordCorrect){
         return res.status(401).json({ message: "Invalid email or password!" });
     }
+    // console.log(isPasswordCorrect);
+    
+    // create jwt token
+    const token = jwt.sign({ userId: userData._id }, process.env.JWT_SECRET);
+
+    // set cookie inside response
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
 
     const { hashedPassword, ...detailsWithoutPassword } = userData.toObject();
     res.status(200).json(detailsWithoutPassword);
 }
+
+
 
 export const logoutUser = async (req, res) => {
     
